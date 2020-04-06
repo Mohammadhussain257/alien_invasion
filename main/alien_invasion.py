51d07498ebf7f1a8ebf7bd10795ca6e6583dc224
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-06 16:04:57 +0545

    Initial commit

---
+++ main/alien_invasion.py
@@ -0,0 +1,10 @@
+import pygame
+import sys
+
+
+class AlienInvasion:
+    """ Overall Class to manage game assets and behavior """
+
+    def __init__(self):
+        """Initialize the game, and create game resources"""
+        pygame.init()
